77ee1b61b54d5a55be4cbec75462cf1c6ff93128
WolfieLeader <koren2318@gmail.com>
2022-09-16 13:56:10 +0300

    Well forgot about it again

--- src/config/error/handleError.ts
+++ src/config/error/handleError.ts
@@ -1,6 +1,6 @@
 import CError from "./CError";
 
-const handleError = (err: any) => {
+const handleError = (err: unknown) => {
   if (err instanceof Error) return new CError(err.message, 500);
   if (err instanceof CError) return err;
   if (typeof err === "string") return new CError(err, 500);
